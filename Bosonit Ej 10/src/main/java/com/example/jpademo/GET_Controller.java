dbf01d44f2c5bc6c2b46371b562f0da28eae97bb
DanielgPolonio <danielg.polonio@bosonit.com>
2021-12-22 19:42:47 +0100

    Ej 10 terminado.

---
+++ Bosonit Ej 10/src/main/java/com/example/jpademo/GET_Controller.java
@@ -0,0 +1,28 @@
+package com.example.jpademo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class GET_Controller {
+    @Autowired
+    UsuarioRepositorio usuarioRepositorio;
+
+    @GetMapping
+    public List<Usuario> getAll(){
+        return usuarioRepositorio.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+
+    public Usuario getById(@PathVariable int id) throws Exception{
+        return usuarioRepositorio.findById(id).orElseThrow(() -> new Exception("No se encuentra"));
+    }
+
+    @GetMapping("name/{name}")
+    public List<Usuario> getByName(@PathVariable String name){
+        return usuarioRepositorio.findByName(name);
+    }
+}
