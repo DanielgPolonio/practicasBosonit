640e3b9c3ed0d08e77d1da473193d4307eababb4
Daniel García <danielg.polonio@bosonit.com>
2022-01-17 17:21:56 +0100

    Heroes Tutorial finalizado.

--- Front/Ej3 Calculadora/main.js
+++ Front/Ej3 Calculadora/main.js
@@ -3,66 +3,31 @@ const MAX_VALOR = 999999999;
 let numeroAnterior = 0;
 let operacion = '';
 window.onload = function () {
-    // ahora asignamos los eventos
-    let elementos = document.getElementsByTagName('button');
-    let numElementos = elementos.length;
-    for (let i = 0; i < numElementos; i++) {
-        let elemento = elementos[i];
-        elemento.onclick = pulsarBotonDirecto;
+    let elements = document.getElementsByTagName('button');
+    let numElements = elements.length;
+    for (let i = 0; i < numElements; i++) {
+        let element = elements[i];
+        element.onclick = clicBoton;
     }
-    var input = document;
-    input.addEventListener('keyup', function (event) {
-        const teclas = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+', '=', 'c', ',', 'x', '\n', '\r', '\n\r'];
-        const len = teclas.length;
-        console.log(event.key);
-        for (let i = 0; i < len; i++) {
-            if (teclas[i] == event.key || event.keyCode == 13) {
-                let data = event.key;
-                if (data == '*')
-                    data = 'x';
-                if (data == '\n' || data == '\r' || data == '\n\r' || event.keyCode == 13)
-                    data = '=';
-                pulsarBoton(event, data);
-            }
-        }
-    });
 };
-function pulsarBotonDirecto(eventos) {
+function clicBoton(eventos) {
     let data = eventos.target.childNodes[0].data;
     if (eventos.target.childNodes[0].data == undefined) {
-        // comprobamos si se pulsa en el boton o en el parrafo
-        //console.log(Number( eventos.target.childNodes[0].childNodes[0].data));
         data = eventos.target.childNodes[0].childNodes[0].data;
     }
-    pulsarBoton(eventos, data);
+    prepararOperacion(eventos, data);
 }
-function pulsarBoton(eventos, data) {
+function prepararOperacion(eventos, data) {
     let numero = Number(data);
-    //console.log(eventos.target);
-    let pantalla = document.getElementById("pantalla");
-    //console.log(numero);
+    let pantalla = document.getElementById("screen");
     if (!isNaN(numero)) {
-        console.log("El el numero: " + numero);
         if (Number(pantalla.innerHTML) == 0)
             pantalla.innerHTML = numero.toString();
         else if (pantalla.innerHTML.length < NUM_DIGITOS)
-            document.getElementById("pantalla").innerHTML += numero;
+            document.getElementById("screen").innerHTML += numero;
     }
     else {
-        console.log("El la letra: " + data);
         numero = Number(pantalla.innerHTML);
-        if (isNaN(numero)) {
-            // aqui hay un caso especial, que el ultimo sea una coma
-            console.log('No es un numero: ' + pantalla.innerHTML);
-            let textoNumero = pantalla.innerHTML;
-            if (textoNumero[textoNumero.length - 1] == '.') {
-                numero = Number(textoNumero.substring(0, textoNumero.length - 1));
-                if (isNaN(numero)) {
-                    numeroAnterior = 0;
-                    numero = 0;
-                }
-            }
-        }
         if (data != '=') {
             if (data != ',') {
                 operacion = data;
@@ -73,76 +38,48 @@ function pulsarBoton(eventos, data) {
                 pantalla.innerHTML += '.';
             }
         }
-        else { // hay que hacer la operacion
-            hazOperacion(pantalla, numero);
+        else {
+            operar(pantalla, numero);
             operacion = '';
             numeroAnterior = 0;
         }
     }
 }
-function hazOperacion(pantalla, numero) {
+function operar(pantalla, numero) {
     let auxCadena = pantalla.innerHTML;
     switch (operacion) {
-        case 'c': {
-            auxCadena = '0';
-            numeroAnterior = 0;
+        case '+': { //Sumar
+            auxCadena = (numero + numeroAnterior).toString();
             break;
         }
-        case 'exp': {
-            console.log('Se hace la potencia');
-            auxCadena = Math.pow(numeroAnterior, numero).toString();
+        case '-': { //Restar
+            auxCadena = (numeroAnterior - numero).toString();
             break;
         }
-        case '/': {
-            console.log('Se divide');
+        case 'x': { //Multiplicar
+            auxCadena = (numeroAnterior * numero).toString();
+            break;
+        }
+        case '/': { //Dividir
             if (numero != 0)
                 auxCadena = (numeroAnterior / numero).toString();
             break;
         }
-        case 'x': {
-            console.log('Se multiplica');
-            auxCadena = (numeroAnterior * numero).toString();
+        case 'c': { //Borrar
+            auxCadena = '0';
+            numeroAnterior = 0;
             break;
         }
-        case '-': {
-            console.log('Se resta');
-            auxCadena = (numeroAnterior - numero).toString();
+        case 'exp': { //Exponencial
+            auxCadena = Math.pow(numeroAnterior, numero).toString();
             break;
         }
-        case '+': {
-            console.log('Se suma');
-            auxCadena = (numero + numeroAnterior).toString();
-            break;
-        }
-        case ',': {
-            console.log('Se hace la coma');
+        case ',': { //Decimal
             auxCadena += '.';
             break;
         }
         default: {
-            console.log('Operación no soportada');
             break;
         }
     }
-    if (auxCadena.length > NUM_DIGITOS) {
-        if (tienePunto(auxCadena))
-            auxCadena = auxCadena.substring(0, NUM_DIGITOS);
-        if (Number(auxCadena) > MAX_VALOR)
-            auxCadena = 'Infinity'; // esto no esta del todo correcto
-    }
-    if (tienePunto(auxCadena)) {
-        let maximo = auxCadena.length - 1;
-        while (auxCadena[maximo] == '0')
-            maximo--;
-        auxCadena = auxCadena.substring(0, maximo + 1);
-    }
-    console.log(auxCadena);
-    pantalla.innerHTML = auxCadena;
-}
-function tienePunto(cadena) {
-    const len = cadena.length;
-    for (let i = 0; i < len; i++)
-        if (cadena[i] == '.')
-            return true;
-    return false;
 }
